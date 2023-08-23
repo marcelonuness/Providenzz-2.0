f8a699d834dbad469b29554c5fc7f9eb7bd219a3
marcelonuness <marcelohenrique.nunes@gmail.com>
2023-08-23 14:52:04 -0700

    commit4

--- app/(routes)/dashboard/page.js
+++ app/(routes)/dashboard/page.js
@@ -1,7 +1,8 @@
-import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    
     <div>
       
     </div>
--- components/sideBar.js
+++ components/sideBar.js
@@ -62,7 +62,7 @@ export default function SideBar() {
                             <span className="ml-3">Finanças</span>    
                             </li>
                         </Link>
-                        <Link href="dashboard/econsulta">
+                        <Link href="/dashboard/econsulta">
                             <li className={`${url === "/dashboard/econsulta" ? "bg-white text-[#1F4D36]" : ""} rounded-md cursor-pointer h-[35px] hover:bg-white hover:text-[#1F4D36] p-1 flex items-center  my-2`}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
@@ -71,7 +71,7 @@ export default function SideBar() {
                             <span className="ml-3">E-consulta</span>    
                             </li>                        
                         </Link>
-                        <Link href="dashboard/integracoes">
+                        <Link href="/dashboard/integracoes">
                             <li className={`${url === "/dashboard/integracoes" ? "bg-white text-[#1F4D36]" : ""} rounded-md cursor-pointer h-[35px] hover:bg-white hover:text-[#1F4D36] p-1 flex items-center  my-2`}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
