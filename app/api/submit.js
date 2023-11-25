3c90fa607eb6f8ade6a7672f01a1c714ae74017a
marcelonuness <marcelohenrique.nunes@gmail.com>
2023-11-25 10:25:34 -0300

    kkk

--- app/api/submit.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body
-        const id = await createItem(data)
-        res.status(200).json({ id })
-      } 
-    
-  }
